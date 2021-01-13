0a8ea14d4901dff1fb9c792d0d0e6383e3fd847b
Alexandru Nisulescu <alex.nisulescu1998@gmail.com>
2021-01-13 11:43:22 +0200

    Implementaare model parserJson
    Am implementat parserul sa citeasca toate datele despre oras
    Datele despre vreme urmeaza sa apara in commitul urmator
    
    Signed-off-by: Alexandru Nisulescu <alex.nisulescu1998@gmail.com>

---
+++ src/main/java/ro/mta/se/lab/model/jsonParser.java
@@ -0,0 +1,57 @@
+package ro.mta.se.lab.model;
+
+
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class jsonParser {
+    private City city=new City();
+    private Weather weather;
+    String weatherIconCode;
+
+    public jsonParser() {
+    }
+
+    public void readWeatherJSON() throws IOException {
+        String data = "";
+        data = new String(Files.readAllBytes(Paths.get("F:\\Ingineria Programarii\\Tema 2 - Weather Forecast App\\weather.json")));
+
+        JsonObject obj = Json.parse(data).asObject();
+
+        city.setName(obj.get("name").asString());
+        city.setLon(obj.get("coord").asObject().get("lon").asFloat());
+        city.setLat(obj.get("coord").asObject().get("lat").asFloat());
+        city.setCountryCode(obj.get("sys").asObject().get("country").asString());
+
+        JsonArray items = Json.parse(data).asObject().get("weather").asArray();
+        weatherIconCode="";
+        weatherIconCode = items.get(0).asObject().getString("icon", "Unknown Item");
+        System.out.println(weatherIconCode);
+
+        city.showDetails();
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public String getWeatherIconCode()
+    {
+        return weatherIconCode;
+    }
+
+    public Weather getWeather() {
+        return weather;
+    }
+
+
+}
